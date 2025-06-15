09392f263c1a4950dd8564bda74e4eb42cb88334
Mohit <mohit07gupta007@gmail.com>
2025-06-15 16:42:15 +0530

    Created 2 new files and 1 package HomeView and AppBar using Scaffold for ui ux and data package has been added

---
+++ app/src/main/java/eu/tutorials/mywishlistapp/HomeView.kt
@@ -0,0 +1,54 @@
+package eu.tutorials.mywishlistapp
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Card
+import androidx.compose.material.SnackbarDefaults.backgroundColor
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import eu.tutorials.mywishlistapp.data.Wish
+
+@Composable
+fun HomeView(){
+    Scaffold(
+        topBar = {AppBarView(title="Wishlist")},
+        floatingActionButton = {
+            FloatingActionButton(modifier = Modifier.padding(24.dp), contentColor = Color.White , backgroundColor = Color.Black, onClick = { /*TODO*/ }) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = null)
+            }
+        }
+    ) {
+        LazyColumn(modifier= Modifier
+            .fillMaxSize()
+            .padding(it)){
+
+        }
+    }
+}
+
+
+@Composable
+fun WishItem(wish: Wish,onClick:()->Unit){
+    Card(modifier= Modifier
+        .fillMaxWidth()
+        .padding(top = 8.dp, start = 8.dp, end = 8.dp)
+        .clickable { onClick() },elevation=10.dp,backgroundColor=Color.White){
+        Column(modifier=Modifier.padding(16.dp)){
+            Text(text=wish.title,fontWeght= FontWeight.ExtraBold)
+            Text(text = wish.description)
+        }
+    }
+}
